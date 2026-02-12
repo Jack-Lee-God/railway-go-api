cae518f29729aa7fec5045867bd52f7a9cc62b74
LiQi <liqi.neurobot@icloud.com>
2026-02-12 20:34:40 +0800

    Update db.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -9,14 +9,35 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("PGHOST"),
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-		os.Getenv("PGDATABASE"),
-		os.Getenv("PGPORT"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		host := os.Getenv("PGHOST")
+		if host == "" {
+			host = "localhost"
+		}
+		user := os.Getenv("PGUSER")
+		if user == "" {
+			user = "postgres"
+		}
+		password := os.Getenv("PGPASSWORD")
+		dbname := os.Getenv("PGDATABASE")
+		if dbname == "" {
+			dbname = "postgres"
+		}
+		port := os.Getenv("PGPORT")
+		if port == "" {
+			port = "5432"
+		}
+
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			host,
+			user,
+			password,
+			dbname,
+			port,
+		)
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
